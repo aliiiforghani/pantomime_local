4ce83444f95ee858b11f7cb4f6aa701e7677eac7
ali forghani <forgani.ali@Gmail.com>
2023-06-10 11:12:46 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-const Application = require("./app/server")
-new Application(2000, "mongodb://127.0.0.1:27017/pantomimedb")
+const Application = require("./app/server");
+new Application(2000, "mongodb://127.0.0.1:27017/pantomimedb");
