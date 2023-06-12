1613c22ce074485c8aa1cfbc0aadb9e8aea9f9b6
ali forghani <forgani.ali@Gmail.com>
2023-06-12 19:46:13 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Application = require("./app/server");
-// new Application(
-//   2000,
-//   "mongodb://root:FmgoSYdnejPPabJoVrr6NW7t@pantomimedb:27017/my-app?authSource=admin"
-// );
-new Application(2000, "mongodb://localhost:27017/pantomimedb2");
+new Application(
+  2000,
+  "mongodb://root:FmgoSYdnejPPabJoVrr6NW7t@pantomimedb:27017/my-app?authSource=admin"
+);
+// new Application(2000, "mongodb://localhost:27017/pantomimedb2");
