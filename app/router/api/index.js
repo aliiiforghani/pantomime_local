2c0cb973c3839e4241e4ea29c6e7ea407ce25270
ali forghani <forgani.ali@Gmail.com>
2023-08-01 11:12:43 +0330

    try to fix cookie problem

--- app/router/api/index.js
+++ app/router/api/index.js
@@ -2,10 +2,8 @@ const { homeContoller } = require("../../http/controller/home.controller");
 const { checkLogin } = require("../../http/middlewares/auth");
 const router = require("express").Router();
 
-router.get("/", checkLogin ,homeContoller.indexPage)
-
-
+router.post("/", checkLogin, homeContoller.indexPage);
 
 module.exports = {
-    HomeRoutes : router
-}
+  HomeRoutes: router,
+};
